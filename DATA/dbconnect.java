ccac43c6d8096bdb2edcf7aaf2994ed23bfe058a
SONLEE2005 <sonsomot123@gmail.com>
2025-04-11 15:41:44 +0700

    dbconnect.java

--- DATA/dbconnect.java
+++ DATA/dbconnect.java
@@ -1,1 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/QuanLyBanXeMay";
+    private static final String JDBC_USER = "root";        
+    private static final String JDBC_PASSWORD = "";
+  
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL driver
+            conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+            System.out.println("Kết nối thành công!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Không tìm thấy JDBC Driver");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Kết nối thất bại!");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
